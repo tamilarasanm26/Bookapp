f1dd0f2fa09fd9d2c850fdf1086b7a76283bf219
Tamilarasan Murugan <tamilmurugan2602@gmail.com>
2024-07-10 14:09:53 +0530

    "model commit"

--- bookapp/src/components/home/Card.jsx
+++ bookapp/src/components/home/Card.jsx
@@ -1,27 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './home.css';
 import Model from './Model';
+import { auth } from '../../firebase/firebase';
 
 function Card({ book }) {
-  console.log(book)
+  const [show,setShow] = useState(false);
+  const [bookItem,setItem] = useState();
+
+ 
   return (
     <>
       {book.map((item, index) => {
         let thumbnail = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
-        let amount = item.saleInfo.listPrice && item.saleInfo.listPrice.amount
-        if(thumbnail!=undefined && amount!=undefined )
+        let amount = item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
+        let author = item.volumeInfo.authors && item.volumeInfo.authors;
+        if(thumbnail!=undefined && amount!=undefined && auth !=undefined)
         {
           return (
            <>
-            <div className='card' key={index}>
+            <div className='card' onClick={()=>{setShow(true),setItem(item)}}>
               <img src={thumbnail}alt=""/>
               <div>
                 <h3 className='title'>{item.volumeInfo.title}</h3>
+                <p className='amt' >{author}</p>
                 <p className='amt'>&#8377;{amount}</p>
               </div>
             </div>
             
-            <Model/>
+            <Model show={show} item={bookItem} onClose={()=>setShow(false)}/>
            </>
           )
         }
