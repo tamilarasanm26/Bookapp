082c3aeeb5640c835af1ab4d9fb4c5d934078a18
Tamilarasan Murugan <tamilmurugan2602@gmail.com>
2024-07-19 15:16:11 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Database connection
-mongoose.connect("mongodb+srv://tamilarasan:tamilarasanm.2631@cluster0.g5a2mwl.mongodb.net/Book", { useNewUrlParser: true, useUnifiedTopology: true })
+// Database connection const apiKey = import.meta.env.VITE_API_KEY;
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
 
