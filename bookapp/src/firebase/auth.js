3a6b5015ed82c16e042f9e41ec43a5cfae19a114
Tamilarasan <121883832+tamilarasanm26@users.noreply.github.com>
2024-08-01 19:35:44 +0530

    Update auth.js

--- bookapp/src/firebase/auth.js
+++ bookapp/src/firebase/auth.js
@@ -13,6 +13,7 @@ export const doSignInWithEmailAndPassword = async(email,password) =>{
 export const doSignInWithGoogle = async() =>{
    const provider = new GoogleAuthProvider();
    const result =  await signInWithPopup(auth,provider);
+
    return result
 }
 
