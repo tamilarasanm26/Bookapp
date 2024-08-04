f80543c5f29314a99d9c19080f290810cb066a61
Tamilarasan <121883832+tamilarasanm26@users.noreply.github.com>
2024-08-04 18:34:54 +0530

    Update Root.jsx

--- bookapp/src/Root.jsx
+++ bookapp/src/Root.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App'; // Adjust the path if necessary
+import App from './App';
 
 const Root = () => (
   <Router>
